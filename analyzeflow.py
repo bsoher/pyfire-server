d5a907864d01dca15f400dfbe49fd0a7907efc85
bsoher <bsoher@briansoher.com>
2024-09-12 21:47:35 -0400

    Updates from Kelvins base repo.

--- analyzeflow.py
+++ analyzeflow.py
@@ -80,7 +80,7 @@ def process(connection, config, metadata):
             waveformGroup.sort(key = lambda item: item.time_stamp)
             ecgData = [item.data for item in waveformGroup if item.waveform_id == 0]
             if len(ecgData) > 0:
-            ecgData = np.concatenate(ecgData,1)
+                ecgData = np.concatenate(ecgData,1)
 
         # Process any remaining groups of raw or image data.  This can 
         # happen if the trigger condition for these groups are not met.
